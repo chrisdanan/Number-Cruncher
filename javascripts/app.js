aa3e438bd3c6821ba28729b464d8752e31984a3c
Christopher Danan <chrisddan@gmail.com>
2015-08-03 12:45:11 -0700

    Fix error where negative numbers were turned to positive numbers.

--- javascripts/app.js
+++ javascripts/app.js
@@ -215,7 +215,7 @@ var main = function(){
 		var inp = $("textarea").val();  //Get the value of string in text area.
 		//inp = inp.replace(/\s/g, '');  //Remove all whitespace from string. Reference: http://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text
 
-		inp = inp.replace(/[^0-9\.\,\ ]+/g, '');  //Remove all non-numeric characters except for points, commas, and whitespace. Reference: http://stackoverflow.com/questions/26202837/remove-non-numeric-characters-except-dash
+		inp = inp.replace(/[^0-9\.\,\ \-]+/g, '');  //Remove all non-numeric characters except for points, commas, whitespace, and minus signs (for negative numbers). Reference: http://stackoverflow.com/questions/26202837/remove-non-numeric-characters-except-dash
 
 		var numbers = inp.split(/[ ,]+/);  //Retrieve each number entered and put them into an array. Reference: http://stackoverflow.com/questions/10346722/how-can-i-split-a-javascript-string-by-white-space-or-comma
 		var length = numbers.length;  //Get how many numbers the user inputted.
