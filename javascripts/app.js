9f675059a75e809f66ecf794c1232fd94622e2d2
Christopher Danan <chrisddan@gmail.com>
2015-08-02 21:40:18 -0700

    Create functions to find mode of the data set and fix problem with spaces being entered.

--- javascripts/app.js
+++ javascripts/app.js
@@ -41,14 +41,12 @@ var median = function(numbers, length){
 	"use strict";
 
 	if(length % 2 === 0){  //Even number of numbers in array.
-		console.log("Even number of numbers in the array.");
 		var rightMid = length / 2,  //Right-middle number.
 			leftMid = rightMid - 1;  //Left-middle number.
 
 		return average([numbers[leftMid], numbers[rightMid]], 2);
 
 	} else{  //Odd number of numbers in array.
-		console.log("Odd number of numbers in the array.");
 		var mid = Math.floor(length / 2);  //Middle of the array.
 		return numbers[mid];
 	}
@@ -56,8 +54,47 @@ var median = function(numbers, length){
 	return null;  //Should not be able to go to this state.
 };
 
-var mode = function(numbers){
+var numOccurrences = function(numbers, length){
+	var currentNum,  //The current number in the array.
+		counts = {};  //The JSON object that holds the number of occurrences of each number.
 
+	//Go through the array, and if the number is already in the counts object, increase its number of occurrence by 1, otherwise put it in object with occurrence of 1.
+	for(var i = 0; i < length; i++){
+		currentNum = numbers[i];
+
+		if(counts.hasOwnProperty(currentNum)){
+			counts[currentNum] += 1;
+		} else{
+			counts[currentNum] = 1;
+		}
+	}
+
+	return mode(counts);
+};
+
+var mode = function(counts){
+	var max = 2,  //Mode has to appear 2 or more times.
+		mode = [];  //Can be more than 2 modes, so put them in array.
+
+	//Go through each key-value pair in counts.
+	for(var num in counts){
+		//Make sure that counts has key.
+		if(counts.hasOwnProperty(num)){
+			if(counts[num] === max){  //If number has same number of occurrences as max, then it is a mode.
+				mode.push(num);
+			} else if(counts[num] > max){  //If number has more number of occurrences as max, then it is new mode.
+				max = counts[num];  //Max occurrences is updated.
+				mode = [];	//Remove previous mode candidates.
+				mode.push(num);  //Put in the new candidate.
+			}
+		}
+	}
+
+	if(mode.length === 0){
+		mode = "None";
+	}
+
+	return mode;
 };
 
 var main = function(){
@@ -70,7 +107,7 @@ var main = function(){
 		var inp = $("textarea").val();  //Get the value of string in text area.
 		//inp = inp.replace(/\s/g, '');  //Remove all whitespace from string. Reference: http://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text
 
-		inp = inp.replace(/[^0-9\.\,]+/g, '');  //Remove all non-numeric characters except for points and commas. Reference: http://stackoverflow.com/questions/26202837/remove-non-numeric-characters-except-dash
+		inp = inp.replace(/[^0-9\.\,\ ]+/g, '');  //Remove all non-numeric characters except for points and commas. Reference: http://stackoverflow.com/questions/26202837/remove-non-numeric-characters-except-dash
 
 		var numbers = inp.split(/[ ,]+/);  //Retrieve each number entered and put them into an array. Reference: http://stackoverflow.com/questions/10346722/how-can-i-split-a-javascript-string-by-white-space-or-comma
 		var length = numbers.length;  //Get how many numbers the user inputted.
@@ -92,6 +129,7 @@ var main = function(){
 		console.log("Sum: " + add(numbers));
 		console.log("Avg: " + average(numbers, length));
 		console.log("Median: " + median(numbers, length));
+		console.log("Mode: " + numOccurrences(numbers, length));
 	});
 };
 
